fb5af1b8cdd57460bbaffb875ea4a7cfaf48d60d
zhong0316 <passion220015@gamil.com>
2018-07-03 00:09:03 +0800

    update repo

---
+++ src/main/java/org/whuims/leetcode/array/NumberOfSubarraysWithBoundedMaximum.java
@@ -0,0 +1,27 @@
+package org.whuims.leetcode.array;
+
+public class NumberOfSubarraysWithBoundedMaximum {
+
+    public static void main(String[] args) {
+        NumberOfSubarraysWithBoundedMaximum numberOfSubarraysWithBoundedMaximum = new NumberOfSubarraysWithBoundedMaximum();
+        int[] A = {2, 1, 4, 3, 2, 5};
+        int res = numberOfSubarraysWithBoundedMaximum.numSubarrayBoundedMax(A, 2, 3);
+        System.out.println(res);
+    }
+
+    public int numSubarrayBoundedMax(int[] A, int L, int R) {
+        int j = 0, count = 0, res = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] >= L && A[i] <= R) {
+                res += i - j + 1;
+                count = i - j + 1;
+            } else if (A[i] < L) {
+                res += count;
+            } else {
+                j = i + 1;
+                count = 0;
+            }
+        }
+        return res;
+    }
+}
