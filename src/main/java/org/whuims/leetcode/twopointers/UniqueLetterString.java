c360d12e298d1851c756f5570bfd8fb0d28c7af7
zhaozhongwei <zhaozhongwei1@jd.com>
2018-07-10 23:44:31 +0800

    commit 3

--- src/main/java/org/whuims/leetcode/twopointers/UniqueLetterString.java
+++ src/main/java/org/whuims/leetcode/twopointers/UniqueLetterString.java
@@ -8,6 +8,7 @@ public class UniqueLetterString {
         UniqueLetterString uniqueLetterString = new UniqueLetterString();
         int res = uniqueLetterString.uniqueLetterString("ABA");
         System.out.println(res);
+        System.out.println("commit 3");
     }
     public int uniqueLetterString(String s) {
         if (s == null || s.length() <= 1) {
